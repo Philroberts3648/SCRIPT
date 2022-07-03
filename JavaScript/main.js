c8e98333fb4f50979b1ba6f85abc231288c6f04d
Philroberts3648 <philip.robertson14@yahoo.com>
2022-07-03 16:28:58 +0100

    Added a script that returns either rock, paper or scisors

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,2 +1,20 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/*
+1. Declear a function variable called computerPlay
+2. Function computerPlay will randomly return 'Rock', 'Paper' or 'Scissors'
+3. 
+*/
+let randomValues;
+function computerPlay(){
+  let randomNumber = Math.floor(Math.random() * 3);
+  console.log(randomNumber);
+  if(randomNumber === 0){
+    randomValues = 'Rock';
+  } else if(randomNumber === 1){
+    randomValues = 'Paper';
+  } else{
+    randomValues = 'Scissors';
+  }
+  return randomValues;
+}
+computerPlay()
+console.log(randomValues);
